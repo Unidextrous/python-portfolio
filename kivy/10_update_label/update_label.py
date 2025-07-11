7b8eb96e2b46269127119320b32362566dc13fa3
Unidextrous <99895694+Unidextrous@users.noreply.github.com>
2025-07-11 19:02:05 -0400

    Add Comments, Add press() Method to Exercise 10

--- kivy/10_update_label/update_label.py
+++ kivy/10_update_label/update_label.py
@@ -2,10 +2,15 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 
-Builder.load_file('update_layout.kv')
+Builder.load_file('update_label.kv')
 
 class MyLayout(Widget):
-    pass
+    def press(self):
+        # Create variables for our widgets
+        name = self.ids.name_input.text
+
+        # Update the label
+        self.ids.name_label.text = name
 
 class UpdateLayoutApp(App):
     def build(self):
